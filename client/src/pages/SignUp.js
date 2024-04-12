1a296ca1c6ed06bd3f7bdab494af2ce5f171f5bf
Alexo <alexsuciultc200@gmail.com>
2024-04-12 22:11:02 +0300

    signin

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,6 +1,73 @@
 import {useEffect, useState} from 'react';
+import {Input, Button} from "@nextui-org/react";
+import {Link} from 'react-router-dom';
+
+export const EyeSlashFilledIcon = (props) => (
+    <svg
+      aria-hidden="true"
+      fill="none"
+      focusable="false"
+      height="1em"
+      role="presentation"
+      viewBox="0 0 24 24"
+      width="1em"
+      {...props}
+    >
+      <path
+        d="M21.2714 9.17834C20.9814 8.71834 20.6714 8.28834 20.3514 7.88834C19.9814 7.41834 19.2814 7.37834 18.8614 7.79834L15.8614 10.7983C16.0814 11.4583 16.1214 12.2183 15.9214 13.0083C15.5714 14.4183 14.4314 15.5583 13.0214 15.9083C12.2314 16.1083 11.4714 16.0683 10.8114 15.8483C10.8114 15.8483 9.38141 17.2783 8.35141 18.3083C7.85141 18.8083 8.01141 19.6883 8.68141 19.9483C9.75141 20.3583 10.8614 20.5683 12.0014 20.5683C13.7814 20.5683 15.5114 20.0483 17.0914 19.0783C18.7014 18.0783 20.1514 16.6083 21.3214 14.7383C22.2714 13.2283 22.2214 10.6883 21.2714 9.17834Z"
+        fill="currentColor"
+      />
+      <path
+        d="M14.0206 9.98062L9.98062 14.0206C9.47062 13.5006 9.14062 12.7806 9.14062 12.0006C9.14062 10.4306 10.4206 9.14062 12.0006 9.14062C12.7806 9.14062 13.5006 9.47062 14.0206 9.98062Z"
+        fill="currentColor"
+      />
+      <path
+        d="M18.25 5.74969L14.86 9.13969C14.13 8.39969 13.12 7.95969 12 7.95969C9.76 7.95969 7.96 9.76969 7.96 11.9997C7.96 13.1197 8.41 14.1297 9.14 14.8597L5.76 18.2497H5.75C4.64 17.3497 3.62 16.1997 2.75 14.8397C1.75 13.2697 1.75 10.7197 2.75 9.14969C3.91 7.32969 5.33 5.89969 6.91 4.91969C8.49 3.95969 10.22 3.42969 12 3.42969C14.23 3.42969 16.39 4.24969 18.25 5.74969Z"
+        fill="currentColor"
+      />
+      <path
+        d="M14.8581 11.9981C14.8581 13.5681 13.5781 14.8581 11.9981 14.8581C11.9381 14.8581 11.8881 14.8581 11.8281 14.8381L14.8381 11.8281C14.8581 11.8881 14.8581 11.9381 14.8581 11.9981Z"
+        fill="currentColor"
+      />
+      <path
+        d="M21.7689 2.22891C21.4689 1.92891 20.9789 1.92891 20.6789 2.22891L2.22891 20.6889C1.92891 20.9889 1.92891 21.4789 2.22891 21.7789C2.37891 21.9189 2.56891 21.9989 2.76891 21.9989C2.96891 21.9989 3.15891 21.9189 3.30891 21.7689L21.7689 3.30891C22.0789 3.00891 22.0789 2.52891 21.7689 2.22891Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+
+  export const EyeFilledIcon = (props) => (
+    <svg
+      aria-hidden="true"
+      fill="none"
+      focusable="false"
+      height="1em"
+      role="presentation"
+      viewBox="0 0 24 24"
+      width="1em"
+      {...props}
+    >
+      <path
+        d="M21.25 9.14969C18.94 5.51969 15.56 3.42969 12 3.42969C10.22 3.42969 8.49 3.94969 6.91 4.91969C5.33 5.89969 3.91 7.32969 2.75 9.14969C1.75 10.7197 1.75 13.2697 2.75 14.8397C5.06 18.4797 8.44 20.5597 12 20.5597C13.78 20.5597 15.51 20.0397 17.09 19.0697C18.67 18.0897 20.09 16.6597 21.25 14.8397C22.25 13.2797 22.25 10.7197 21.25 9.14969ZM12 16.0397C9.76 16.0397 7.96 14.2297 7.96 11.9997C7.96 9.76969 9.76 7.95969 12 7.95969C14.24 7.95969 16.04 9.76969 16.04 11.9997C16.04 14.2297 14.24 16.0397 12 16.0397Z"
+        fill="currentColor"
+      />
+      <path
+        d="M11.9984 9.14062C10.4284 9.14062 9.14844 10.4206 9.14844 12.0006C9.14844 13.5706 10.4284 14.8506 11.9984 14.8506C13.5684 14.8506 14.8584 13.5706 14.8584 12.0006C14.8584 10.4306 13.5684 9.14062 11.9984 9.14062Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
 
 const SignUp = () => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const toggleVisibility = () => setIsVisible(!isVisible);
+
+    const [isVisible2, setIsVisible2] = useState(false);
+
+    const toggleVisibility2 = () => setIsVisible2(!isVisible2);
+
+
     const [last_name, setLast_name] = useState('');
     const [first_name, setFirst_name] = useState('');
     const [password, setPassword] = useState('');
@@ -24,11 +91,82 @@ const SignUp = () => {
             </div>
             }
             {role &&
-            <div className='signUp-div'>
+            <form className='signUp-div'>
                 <p className='signUp-create-account-text'>
                     CREATE ACCOUNT
                 </p>
-            </div>
+                <div className='inputs-div'>
+                    <div style={{display:'flex', flexDirection:'row', justifyContent:'space-between'}}>
+                        <div style={{width:'49%'}}>
+                            <Input type="email" variant="bordered" label="First Name" />
+                        </div>
+                        <div style={{width:'49%'}}>
+                            <Input
+                            label="Password"
+                            variant="bordered"
+                            endContent={
+                                <button className="focus:outline-none" type="button" onClick={toggleVisibility}>
+                                {isVisible ? (
+                                    <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                                ) : (
+                                    <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                                )}
+                                </button>
+                            }
+                            type={isVisible ? "text" : "password"}
+                            />
+                        </div>
+                    </div>
+                    <div style={{display:'flex', flexDirection:'row', justifyContent:'space-between'}}>
+                        <div style={{width:'49%'}}>
+                            <Input type="email" variant="bordered" label="Last Name" />
+                        </div>
+                        <div style={{width:'49%'}}>
+                            <Input
+                            label="Confirm password"
+                            variant="bordered"
+                            endContent={
+                                <button className="focus:outline-none" type="button" onClick={toggleVisibility2}>
+                                {isVisible2 ? (
+                                    <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                                ) : (
+                                    <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                                )}
+                                </button>
+                            }
+                            type={isVisible2 ? "text" : "password"}
+                            />
+                        </div>
+                    </div>
+                    <Input type="email" variant="bordered" label="Email" />
+                    <Input type="email" variant="bordered" label="Username" />
+                </div>
+                <div className="or-sign-with">
+                    <div className="or-sign-with-line"> </div>
+                        <p style={{padding:'7px'}}>Or </p>
+                    <div className="or-sign-with-line"> </div>
+                </div>
+                <div className='login-icons'>
+                    <svg style={{margin:'0 auto'}} cursor="pointer" viewBox="0 -28.5 256 256" width="55" height="55" version="1.1" preserveAspectRatio="xMidYMid" fill="#000000">
+                    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                    <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+                    <g id="SVGRepo_iconCarrier">
+                        <g>
+                        <path d="M216.856339,16.5966031 C200.285002,8.84328665 182.566144,3.2084988 164.041564,0 C161.766523,4.11318106 159.108624,9.64549908 157.276099,14.0464379 C137.583995,11.0849896 118.072967,11.0849896 98.7430163,14.0464379 C96.9108417,9.64549908 94.1925838,4.11318106 91.8971895,0 C73.3526068,3.2084988 55.6133949,8.86399117 39.0420583,16.6376612 C5.61752293,67.146514 -3.4433191,116.400813 1.08711069,164.955721 C23.2560196,181.510915 44.7403634,191.567697 65.8621325,198.148576 C71.0772151,190.971126 75.7283628,183.341335 79.7352139,175.300261 C72.104019,172.400575 64.7949724,168.822202 57.8887866,164.667963 C59.7209612,163.310589 61.5131304,161.891452 63.2445898,160.431257 C105.36741,180.133187 151.134928,180.133187 192.754523,160.431257 C194.506336,161.891452 196.298154,163.310589 198.110326,164.667963 C191.183787,168.842556 183.854737,172.420929 176.223542,175.320965 C180.230393,183.341335 184.861538,190.991831 190.096624,198.16893 C211.238746,191.588051 232.743023,181.531619 254.911949,164.955721 C260.227747,108.668201 245.831087,59.8662432 216.856339,16.5966031 Z M85.4738752,135.09489 C72.8290281,135.09489 62.4592217,123.290155 62.4592217,108.914901 C62.4592217,94.5396472 72.607595,82.7145587 85.4738752,82.7145587 C98.3405064,82.7145587 108.709962,94.5189427 108.488529,108.914901 C108.508531,123.290155 98.3405064,135.09489 85.4738752,135.09489 Z M170.525237,135.09489 C157.88039,135.09489 147.510584,123.290155 147.510584,108.914901 C147.510584,94.5396472 157.658606,82.7145587 170.525237,82.7145587 C183.391518,82.7145587 193.761324,94.5189427 193.539891,108.914901 C193.539891,123.290155 183.391518,135.09489 170.525237,135.09489 Z" fill="#5865F2" fillRule="nonzero"> </path>
+                        </g>
+                    </g>
+                    </svg>
+                </div>
+                <div className='submit-div'>
+                    <Button color="default" type = "submit"
+                    variant="bordered" size='lg' className="btnSgnUp">
+                        Submit
+                    </Button>
+                    <p className='signText'>
+                        Already registered? <Link to="/sign-in" className="change-toInUp"> Sign-In </Link> 
+                    </p>
+                </div>    
+            </form>
             }
         </div>
     );
