aba149edeaf3ed2c6f7909d5bc5bc858c29f50d2
xndadelin <adelinmihai071@gmail.com>
2024-04-12 18:21:09 +0300

    init

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import { Card, CardHeader, NextUIProvider } from "@nextui-org/react";
+function App() {
+  return (
+    <NextUIProvider>
+      <div>
+        <Card>
+          <CardHeader>
+            aslkdjasdjlaskdjl
+          </CardHeader>
+        </Card>
+      </div>
+    </NextUIProvider>
+  );
+}
+
+export default App;
---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+require('dotenv').config()
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.listen(8080, () => {
+    mongoose.connect(process.env.MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+        console.log('App is running and database connected')
+    })
+})
