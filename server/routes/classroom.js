45dc6252b3b5f49a5e06f88da3f305058a600968
xndadelin <adelinmihai071@gmail.com>
2024-04-13 12:25:56 +0300

    view classroom, get classes

--- server/routes/classroom.js
+++ server/routes/classroom.js
@@ -89,12 +89,39 @@ router.post('/join/:id', async(req, res) => {
         if(classroomData.students.includes(user.username)){
             return res.status(400).json({success: false, error: 'You have already joined this classroom'})
         }
-        user.classrooms.push(classroomId)
+        user.classrooms.push(classroomData)
         await user.save()
         await classroom.findOneAndUpdate({_id: classroomId}, {$push: {students: user.username}})
         return res.status(200).json({success: true, message: 'You have joined the classroom'})
     }catch(err){
         return res.status(500).json({success: false, error: 'Internal Server Error'})
+    }
+})
+router.get('/myclasses/:username', async(req, res) => {
+    try{
+        const {username} = req.params
+        if(!req.headers.cookie){
+            return res.status(401).json({success: false, error: 'Unauthorized'})
+        }
+        const cookies = cookie.parse(req.headers.cookie)
+        if(!cookies || !cookies.primaryToken || !cookies.refreshToken){
+            return res.status(401).json({success: false, error: 'Unauthorized'})
+        }
+        const {user} = await getUser(cookies.primaryToken, cookies.refreshToken)
+        if(!user){
+            return res.status(401).json({success: false, error: 'Unauthorized'})
+        }
+        if(user.username !== username){
+            return res.status(401).json({success: false, error: 'Unauthorized'})
+        }
+        const usera = await User.findOne({username}).select('-id -password')
+        if(!usera){
+            return res.status(404).json({success: false, error: 'User not found'})
+        }
+        const classrooms = await classroom.find({students: user.username}).select('-students -password')
+        return res.status(200).json({success: true, classrooms})
+    }catch(err){
+        return res.status(500).json({success: false, error: err.message})
     }
 })
 module.exports = router
