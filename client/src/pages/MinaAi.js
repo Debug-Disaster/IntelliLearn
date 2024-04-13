511af82649c02dce129b491b1a93436d5ca4b55b
Alexo <alexsuciultc200@gmail.com>
2024-04-14 00:48:45 +0300

    mina ai bug fix

--- client/src/pages/MinaAi.js
+++ client/src/pages/MinaAi.js
@@ -1,4 +1,4 @@
-import {Button, Input} from "@nextui-org/react";
+import {Button, Input, ScrollShadow} from "@nextui-org/react";
 import LiveHelpIcon from '@mui/icons-material/LiveHelp';
 import SettingsIcon from '@mui/icons-material/Settings';
 import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
@@ -7,14 +7,13 @@ import {useContext} from 'react'
 import {UserContext} from '../context/UserContext'
 import NotFound from "./NotFound";
 import {useState} from 'react'
-import { ScrollShadow } from "@nextui-org/react";
 const MinaAi = () => {
     const {user} = useContext(UserContext);
     const [prompt, setPrompt] = useState('')
     const [prompts, setPrompts] = useState([])
+    const [hiddenOnClick, setHiddenOnClick] = useState(false);
     if(!user) return <NotFound/>
     //loading, save prompts, display prompts (user prompt, mina prompt, user prompt, mina prompt, ...)
-    //sa para ca promptul este de la noi si celalalt de la minaAI
     const getPrompt = async() => {
         prompts.push(prompt)
         try{
@@ -32,6 +31,7 @@ const MinaAi = () => {
             if(res.success === false){
                 console.log(data.error)
             }else{
+                console.log(data)
                 setPrompts([...prompts, data.message])
             }
         }catch(err){
@@ -120,61 +120,73 @@ const MinaAi = () => {
                 </div>
             </div>
             <div className="chat-minaAi">
-                    <div className="w-[80%] mx-auto">
-                        {user ?
-                        <p style={{fontSize:'2rem'}}>
-                            Welcome, {user.username}!
-                        </p>
-                        : 
-                        <p style={{fontSize:'2rem'}}>
-                            Welcome, Newbie!
-                        </p>
-                        }
-                        <p style={{fontSize:'1.2rem'}}>
-                            Mina ignites minds with wisdom, making everything brighter...
-                        </p>
-                            {prompts.length === 0 && (
-                                <div className="flex w-[100%] mx-auto flex-row justify-evenly mt-12 mina-contains-prompt-ideas">
-                                <div className="prompt-ideas-mina" style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end' }}>
-                                    <div className="pl-4 pt-2 pr-4">
-                                        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                    </div>
-                                    <SendIcon className="pb-3 pr-4 iconitaSend" style={{width:'40px', height:'40px', cursor:'pointer'}}/>
-                                </div>
-                                <div className="prompt-ideas-mina" style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end' }}>
-                                    <div className="pl-4 pt-2 pr-4">
-                                        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                    </div>
-                                    <SendIcon className="pb-3 pr-4 iconitaSend" style={{width:'40px', height:'40px', cursor:'pointer'}}/>
-                                </div>
-                                <div className="prompt-ideas-mina prompt-hidden-mina" style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end' }}>
-                                    <div className="pl-4 pt-2 pr-4">
-                                        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                    </div>
-                                    <SendIcon className="pb-3 pr-4" style={{width:'40px', height:'40px', cursor:'pointer'}}/>
-                                </div>
-                                <div className="prompt-ideas-mina prompt-hidden-mina" style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end' }}>
-                                    <div className="pl-4 pt-2 pr-4">
-                                        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                    </div>
-                                    <SendIcon className="pb-3 pr-4" style={{width:'40px', height:'40px', cursor:'pointer'}}/>
-                                </div>
-                            </div>
-                        )}
-                        <ScrollShadow>
-                            <div className="flex flex-col gap-5 mt-5 h-[400px]">
-                                {prompts.map((prompt, index) => (
-                                        <div className="bg-zinc-800 rounded-md">
-                                            <p className="p-3">{prompt}</p>
+                <div className="w-[80%] mx-auto">
+                    {user ?
+                    <p style={{fontSize:'2rem'}}>
+                        Welcome, {user.username}!
+                    </p>
+                    : 
+                    <p style={{fontSize:'2rem'}}>
+                        Welcome, Newbie!
+                    </p>
+                    }
+                    <p style={{fontSize:'1.2rem'}}>
+                        Mina ignites minds with wisdom, making everything brighter...
+                    </p>
+                    {prompts.length == 0 && (
+                                            <div className="flex w-[100%] mx-auto flex-row justify-evenly mt-12 mina-contains-prompt-ideas">
+                                            <div className="prompt-ideas-mina" style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end' }}>
+                                                <div className="pl-4 pt-2 pr-4">
+                                                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                                </div>
+                                                <SendIcon className="pb-3 pr-4 iconitaSend" style={{width:'40px', height:'40px', cursor:'pointer'}}/>
+                                            </div>
+                                            <div className="prompt-ideas-mina" style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end' }}>
+                                                <div className="pl-4 pt-2 pr-4">
+                                                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                                </div>
+                                                <SendIcon className="pb-3 pr-4 iconitaSend" style={{width:'40px', height:'40px', cursor:'pointer'}}/>
+                                            </div>
+                                            <div className="prompt-ideas-mina prompt-hidden-mina" style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end' }}>
+                                                <div className="pl-4 pt-2 pr-4">
+                                                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                                </div>
+                                                <SendIcon className="pb-3 pr-4" style={{width:'40px', height:'40px', cursor:'pointer'}}/>
+                                            </div>
+                                            <div className="prompt-ideas-mina prompt-hidden-mina" style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end' }}>
+                                                <div className="pl-4 pt-2 pr-4">
+                                                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                                </div>
+                                                <SendIcon className="pb-3 pr-4" style={{width:'40px', height:'40px', cursor:'pointer'}}/>
+                                            </div>
                                         </div>
-                                    ))}
-                            </div>
-                        </ScrollShadow>
-                    <Input value={prompt} onChange={(e) => setPrompt(e.target.value)} type="email" size="lg" variant="underlined" label="Message Mina" className="w-[80%] mx-auto"
-                    endContent={
-                        <Button onClick={() => {getPrompt(); setPrompt("")}} size="small" auto icon={<SendIcon/>} className="bg-[#1e90ff] cursor-pointer">Send</Button>
-                    }/>
+                    )}
+                    <ScrollShadow>
+                       {prompts.length !== 0 ? (
+                         <div className="flex flex-col gap-5 mt-5 h-[500px]">
+                         {prompts.map((prompt, index) => (
+                                 <div className="bg-zinc-800 rounded-md">
+                                     <p className="p-3">
+                                         <pre style={{whiteSpace: 'break-spaces'}}>{prompt}</pre>
+                                     </p>
+                                 </div>
+                             ))}
+                        </div>
+                       ) : <div></div>}
+                    </ScrollShadow>
                 </div>
+                <Input value={prompt} onChange={(e) => setPrompt(e.target.value)} type="email" size="lg" variant="underlined" label="Message Mina" className="w-[80%] mx-auto"
+                endContent={
+                    <Button onClick={() => {getPrompt(); setPrompt("")}} size="small" auto icon={<SendIcon/>} className="bg-[#1e90ff] cursor-pointer">Send</Button>
+                }
+                onKeyDown={(e) => {
+                    if (e.key === 'Enter') {
+                        e.preventDefault();
+                        getPrompt();
+                        setPrompt("");
+                    }
+                }}
+                />
             </div>
         </div>
     );
